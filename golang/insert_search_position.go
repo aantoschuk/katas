c22735a967d7ece3d1c746b82a8caed4eaf484a6
Alexander Antoschuk <alexander.antoschuk.dev@gmail.com>
2025-03-17 12:05:35 +0200

    insert search position

---
+++ golang/insert_search_position.go
@@ -0,0 +1,34 @@
+// Given a sorted array of integers and a target value,
+// return the index if the target is found
+// if not, return the index where it would be if it were inserted in order
+package main
+
+func insertSearchPosition(nums []int, target int) int {
+	for i, v := range nums {
+		if v >= target {
+			return i
+		}
+	}
+
+	return len(nums)
+}
+
+// since it was stated that the problem should be solved with O(log n) complexity,
+// I used binary search.
+func InsertSearchPosition(nums []int, target int) int {
+	low, high := 0, len(nums)
+
+	// while loop
+	for low <= high {
+		// find  mid of the array.
+		mid := low + (high-low)/2
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] < target {
+			low = mid + 1
+		} else {
+			high = mid - 1
+		}
+	}
+	return low
+}
---
+++ golang/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{1, 3 }
+
+	res := InsertSearchPosition(nums, 3)
+
+	fmt.Println(res)
+}
